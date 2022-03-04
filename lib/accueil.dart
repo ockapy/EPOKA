784a9836fc64b4635202247f64fe13b1d1bcd3ee
ockapy <maxime.rodriguez.st@gmail.com>
2022-03-04 10:40:51 +0100

    initial commit

---
+++ lib/accueil.dart
@@ -0,0 +1,19 @@
+// import 'dart:html';
+
+// import 'package:flutter/material.dart';
+
+// class HomePage extends StatefulWidget {
+//   @override
+//   @override
+//   State<HomePage> createState() => HomePageState();
+// }
+
+// class HomePageState extends State<HomePage> {
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: PreferredSize(child: ),
+//     )
+//   }
+// }
